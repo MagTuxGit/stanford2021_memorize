3b343e782c3493a39675b7e17b201f4f8c5d5720
Andriy Trubchanin <magtuxgit@gmail.com>
2022-07-01 16:45:46 +0300

    hstack

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,21 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        /// fill() by default
-        //return RoundedRectangle(cornerRadius: 20).fill()
-        //.padding(16)
-        
-        // return ZStack(content: {})
-        
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, Paul!!")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
             .padding(.horizontal)
             .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
     }
 }
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20).stroke(lineWidth: 3)
+            Text("Hello, Paul!!")
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
