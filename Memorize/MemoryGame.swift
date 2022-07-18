23e1d3143bc6afc88a4a5f9a9eb4be95e1eda433
Andriy Trubchanin <magtuxgit@gmail.com>
2022-07-18 17:36:50 +0300

    lecture 4: game logic

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,17 +7,30 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: [Card]
     
-    mutating func choose(_ card: Card) {
-        if let cardIndex = index(of: card) {
-            cards[cardIndex].isFaceUp.toggle()
-        }
-    }
+    private var facedUpCardIndex: Int?
     
-    func index(of card: Card) -> Int? {
-        return cards.firstIndex(where: { $0.id == card.id })
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+            !cards[chosenIndex].isFaceUp,
+            !cards[chosenIndex].isMatched
+        {
+            if facedUpCardIndex != nil {
+                if (cards[facedUpCardIndex!].content == cards[chosenIndex].content) {
+                    cards[facedUpCardIndex!].isMatched = true
+                    cards[chosenIndex].isMatched = true
+                }
+                facedUpCardIndex = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                facedUpCardIndex = chosenIndex
+            }
+            cards[chosenIndex].isFaceUp.toggle()
+        }
     }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
@@ -30,10 +43,10 @@ struct MemoryGame<CardContent> {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
-
+        
         var id: Int
     }
 }
